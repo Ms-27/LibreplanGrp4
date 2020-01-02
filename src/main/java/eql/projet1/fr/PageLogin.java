07bb019a2aab4a2ea43a0b5fcf471426ecb3b547
Ms-27 <ms@juliver.com>
2020-01-02 15:17:29 +0100

    Rédaction TestConnect

--- src/main/java/eql/projet1/fr/PageLogin.java
+++ src/main/java/eql/projet1/fr/PageLogin.java
@@ -1,9 +1,11 @@
 package eql.projet1.fr;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class PageLogin {
+public class PageLogin extends PageAbstractMenu {
 	@FindBy (xpath="//input[@id='button']")
 	WebElement seconnecter_btn;
 	
@@ -13,7 +15,10 @@ public class PageLogin {
 	@FindBy (xpath="//input[@id='textfield2']")
 	WebElement mdp_field;
 	
-	public PageIndex connect() {
-		
+	public PageIndex connect(WebDriver d, String s, String p) {
+		TechnicalTools.fillFiels(utilisateur_field, s);
+		TechnicalTools.fillFiels(mdp_field, p);
+		seconnecter_btn.click();
+		return PageFactory.initElements(d, PageIndex.class);
 	}
 }
--- src/test/java/eql/projet1/fr/TestConnect_OBO.java
+++ src/test/java/eql/projet1/fr/TestConnect_OBO.java
@@ -14,6 +14,8 @@ import org.openqa.selenium.support.PageFactory;
 public class TestConnect_OBO {
 	
 	WebDriver driver;
+	String login = "admin";
+	String pswd = "admin";
 	
 	@Before
 	public void setUp() {
@@ -32,8 +34,10 @@ public class TestConnect_OBO {
 		assertEquals("Erreur titre de la page", "LibrePlan: accès utilisateur", driver.getTitle());
 		
 		PageLogin page_login = PageFactory.initElements(driver, PageLogin.class);
-		
-		
+		PageIndex page_index = page_login.connect(driver, login, pswd);
+		assertTrue(page_index.user_txt.isDisplayed());
+		assertTrue(page_index.user_txt.getText().contains("admin"));
+		page_index.signout_btn.click();
 	}
 
 }

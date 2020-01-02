6bfb0bca282fc21a0ee9128ea14811f0d7a911e6
Ms-27 <ms@juliver.com>
2020-01-02 10:53:26 +0100

    création de fichiers empty pour arbo

---
+++ src/test/java/eql/projet1/fr/emptyTC.java
@@ -0,0 +1,11 @@
+package eql.projet1.fr;
+
+import junit.framework.TestCase;
+
+public class emptyTC extends TestCase {
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}

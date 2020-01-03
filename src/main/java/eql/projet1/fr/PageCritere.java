995d45d2b36a4e21d379de7a6ba14fae373af595
Sedatyf <gaxel@hotmail.fr>
2020-01-03 11:03:29 +0100

    Update PageCritere and Test_CRI_01

--- src/main/java/eql/projet1/fr/PageCritere.java
+++ src/main/java/eql/projet1/fr/PageCritere.java
@@ -1,5 +1,41 @@
 package eql.projet1.fr;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PageCritere {
+
+	@FindBy(xpath = "//div[contains(text(), 'critère')]")
+	WebElement criteres_header;
+	
+	@FindBy(xpath = "//span[contains(text(), 'Modifier')]")
+	WebElement modifier_tab;
+
+	@FindBy(xpath = "//div[contains(text(), 'Code')]")
+	WebElement code_th;
+
+	@FindBy(xpath = "//div[contains(text(), 'Nom')]")
+	WebElement nom_th;
+
+	@FindBy(xpath = "//div[contains(text(), 'Type')][contains(@class, 'column-cnt')]")
+	WebElement type_th;
+
+	@FindBy(xpath = "//div[contains(text(), 'Activé')]")
+	WebElement active_th;
+
+	@FindBy(xpath = "//div[contains(text(), 'Opération')]")
+	WebElement operation_th;
+
+	@FindBy(xpath = "//td[contains(text(), 'Créer')]")
+	WebElement creer_btn;
+	
+	@FindBy(xpath = "//td[contains(text(), 'Enregistrer')]")
+	WebElement enregistrer_btn;
+	
+	@FindBy(xpath = "//td[contains(text(), 'Sauver')]")
+	WebElement sauver_btn;
+	
+	@FindBy(xpath = "//td[contains(text(), 'Annuler')]")
+	WebElement annuler_btn;
 
 }
--- src/test/java/eql/projet1/fr/Test_CRI_01_CGA.java
+++ src/test/java/eql/projet1/fr/Test_CRI_01_CGA.java
@@ -8,10 +8,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class Test_CRI_01_CGA {
 	
 	WebDriver driver;
+	
+	//JDD
+	String login = "admin";
+	String password = "admin";
 	
 	@Before
 	public void startup() {
@@ -26,7 +31,25 @@ public class Test_CRI_01_CGA {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		driver.get("http://localhost:8090/libreplan/");
+		assertEquals("Erreur titre de la page", "LibrePlan: accès utilisateur", driver.getTitle());
+		
+		PageLogin page_login = PageFactory.initElements(driver, PageLogin.class);
+		PageIndex page_index = page_login.connect(driver, login, password);
+		assertEquals("Erreur Ressources non présent", "Ressources ", page_index.ressources_btn.getText());
+		
+		PageCritere page_critere = page_index.accesCritere(driver);
+		assertEquals("La page Critere ne c'est pas chargé", "Types de critères Liste", page_critere.criteres_header.getText());
+		assertEquals("Le tableau n'est pas correct", "Nom", page_critere.nom_th.getText());
+		assertEquals("Le tableau n'est pas correct", "Code", page_critere.code_th.getText());
+		assertEquals("Le tableau n'est pas correct", "Type", page_critere.type_th.getText());
+		assertEquals("Le tableau n'est pas correct", "Activé", page_critere.active_th.getText());
+		assertEquals("Le tableau n'est pas correct", "Opérations", page_critere.operation_th.getText());
+		assertTrue("Le bouton Créer n'existe pas", page_critere.creer_btn.isEnabled());
+		
+		page_critere.creer_btn.click();
+		assertTrue("Le bouton Enregistrer n'est pas présent", page_critere.enregistrer_btn.isEnabled());
+		
 	}
 
 }

cfd2a9c7279add2d842a27c74b75591c0a6741c1
unknown <sarah_bali10@yahoo.fr>
2020-01-03 15:58:51 +0100

    modification Test AVA 01 SBA step 3

--- src/test/java/eql/projet1/fr/Test_AVA_01_SBA.java
+++ src/test/java/eql/projet1/fr/Test_AVA_01_SBA.java
@@ -1,8 +1,6 @@
 package eql.projet1.fr;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,6 +15,8 @@ public class Test_AVA_01_SBA {
 	WebDriver driver;
 	String login = "admin";
 	String pswd = "admin";
+	String val_max = "100,00";
+	String precision = "0,1000";
 	
 	@Before
 	public void startup() {
@@ -50,14 +50,30 @@ public class Test_AVA_01_SBA {
 				assertTrue(page_typeavancement.creer_btn.isDisplayed());
 				
 				assertTrue("Le bouton Créer n'est pas présent", page_typeavancement.creer_btn.isEnabled());
+				
+				
+				
 				// création d'un type d'avancement
 				page_typeavancement.creer_btn.click();
 				
+				assertEquals("Le Nom d'unité n'est pas vide","",page_typeavancement.nom_unite_input.getText());
+				assertTrue("La case Actif n'est pas cochée par défaut",page_typeavancement.actif_checkbox.isEnabled());
+				assertEquals("La valeur par defaut de la valeur max n'est pas celle attendue",val_max,page_typeavancement
+						.val_max_input.getAttribute("value"));
+				assertEquals("La valeur par defaut de la précision n'est pas celle attendue",precision,page_typeavancement
+						.precision_input.getAttribute("value"));
+								
+				System.out.println(page_typeavancement.user_input.getAttribute("value"));
+				assertEquals("La valeur User est modifiable",null,page_typeavancement.user_input.getAttribute("value"));
+				assertFalse("La case pourcentage n'est pas cochée",page_typeavancement.pourcentage_checkbox.isSelected());
 				
-	
+				
 				assertTrue("Le bouton Enregistrer n'est pas présent", page_typeavancement.enregistrer_btn.isEnabled());
 				assertTrue("Le bouton Sauver et continuer n'est pas présent", page_typeavancement.sauver_btn.isEnabled());
 				assertTrue("Le bouton Sauver et continuer n'est pas présent", page_typeavancement.annuler_btn.isEnabled());
+				
+				
+				// création d'un type d'avancement - sans pourcentage 
 				
 	}
 	
